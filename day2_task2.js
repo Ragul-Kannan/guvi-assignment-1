8badb7b6f388f2aebf619f12c448c734e1b80b1b
Ragul-Kannan <97528740+Ragul-Kannan@users.noreply.github.com>
2022-01-22 17:52:16 +0400

    Add files via upload

---
+++ day2_task2.js
@@ -0,0 +1,21 @@
+//Day2_Task2
+
+console.log(typeof 1);
+console.log(typeof 1.1)
+console.log(typeof '1.1')
+console.log(typeof true)
+console.log(typeof null )
+console.log(typeof undefined)
+console.log(typeof [])
+console.log(typeof {})
+console.log(typeof NaN)
+
+
+
+// Description: 
+// anything inside ""or '' is a string
+// number is a number 
+// []and{} are objects
+// null is an object
+// undefined means no value is assigned (undefined is assigned by the interpreter)
+// NaN means it is not a proper number
